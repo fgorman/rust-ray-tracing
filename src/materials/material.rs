8ecfcc35c47b41b045344966ad5adb6b1688c2ba
Foster Gorman <mh337.fg@gmail.com>
2022-10-26 20:14:32 -0400

    Add ability to create generic materials

--- src/materials/material.rs
+++ src/materials/material.rs
@@ -1,7 +1,19 @@
+use std::rc::Rc;
+
 use crate::ray::Ray;
 use crate::hittables::hittable::HitRecord;
 use crate::vec3::Color;
 
-pub trait Material {
-    fn scatter(r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool;
+pub trait Scatter {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool;
+}
+
+pub struct Material {
+    pub mat_type: Rc<dyn Scatter>,
+}
+
+impl Scatter for Material {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
+        self.mat_type.scatter(r_in, rec, attenuation, scattered)
+    }
 }
